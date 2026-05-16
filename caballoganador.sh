1c6da4151f4b861aa37511af6cab4718656ebff7
abraham <abrahamarke@gmail.com>
2026-05-16 14:57:54 +0200

    update

--- caballoganador.sh
+++ caballoganador.sh
@@ -14,10 +14,9 @@ echo "=========================================="
 echo "     GENERADOR COMPLETO DNS BIND9"
 echo "=========================================="
 
-read -p "Numero de dominios: " NUM_DOM
+d=1
 
-for ((d=1; d<=NUM_DOM; d++))
-do
+while true; do
 
     echo
     echo "=========== DOMINIO PRINCIPAL $d ==========="
@@ -33,8 +32,8 @@ do
     echo
     echo "===== MASTER DNS ====="
 
-    read -p "Hostname master DNS: " HOST_MASTER
-    read -p "Alias master DNS: " ALIAS_MASTER
+    read -p "Hostname master DNS (ej: serverdns1): " HOST_MASTER
+    read -p "Alias master DNS (ej: masterdns): " ALIAS_MASTER
     read -p "IP master DNS: " IP_MASTER
 
     #################################################
@@ -44,15 +43,16 @@ do
     echo
     echo "===== SLAVE DNS DOMINIO PRINCIPAL ====="
 
-    read -p "Hostname slave DNS: " HOST_SLAVE
-    read -p "Alias slave DNS: " ALIAS_SLAVE
+    read -p "Hostname slave DNS (ej: serverdns2): " HOST_SLAVE
+    read -p "Alias slave DNS (ej: slavedns): " ALIAS_SLAVE
     read -p "IP slave DNS en red principal: " IP_SLAVE_MAIN
 
     #################################################
     # ADMIN
     #################################################
-
-    read -p "Correo administrador (ej: master.gmail.com): " ADMIN
+    echo
+    echo "ATENCION: El correo debe llevar un punto en vez de @ y terminar en punto."
+    read -p "Correo administrador (ej: master.gmail.com.): " ADMIN
 
     INVERSA=$(echo $RED | awk -F. '{print $3"."$2"."$1}')
 
@@ -70,18 +70,20 @@ zone "$DOM"
 {
     type master;
     file "/etc/bind/zonas/db.$DOM";
-    allow-query { 127.0.0.1; $RED.0/$MASK; };
+    allow-query { any; }; 
     allow-transfer { $IP_SLAVE_MAIN; };
     also-notify { $IP_SLAVE_MAIN; };
+    notify yes;
 };
 
 zone "$INVERSA.in-addr.arpa"
 {
     type master;
     file "/etc/bind/zonas/db.$RED";
-    allow-query { 127.0.0.1; $RED.0/$MASK; };
+    allow-query { any; };
     allow-transfer { $IP_SLAVE_MAIN; };
     also-notify { $IP_SLAVE_MAIN; };
+    notify yes;
 };
 
 EOF
@@ -100,16 +102,16 @@ zone "$DOM"
 {
     type slave;
     file "/etc/bind/zonas/db.$DOM";
-    masters {$IP_MASTER;};
-    allow-query { 127.0.0.1; $RED.0/$MASK; };
+    masters { $IP_MASTER; };
+    allow-query { any; };
 };
 
 zone "$INVERSA.in-addr.arpa"
 {
     type slave;
     file "/etc/bind/zonas/db.$RED";
-    masters {$IP_MASTER;};
-    allow-query { 127.0.0.1; $RED.0/$MASK; };
+    masters { $IP_MASTER; };
+    allow-query { any; };
 };
 
 EOF
@@ -122,20 +124,26 @@ EOF
 
     cat > $DIRECTO <<EOF
 \$TTL 604800
+@   IN  SOA $DOM. $ADMIN (
+            2          ; Serial
+            604800     ; Refresh
+            86400      ; Retry
+            2419200    ; Expire
+            604800 )   ; Negative Cache TTL
 
-@   IN  SOA $DOM. $ADMIN. (
-            1
-            604800
-            86400
-            2419200
-            604800 )
-
+; Name Servers
+@   IN  NS  $HOST_MASTER.$DOM.
 @   IN  NS  $HOST_SLAVE.$DOM.
 
-$HOST_MASTER   IN  A   $IP_MASTER
-$ALIAS_MASTER  IN  CNAME $HOST_MASTER
+; Resolucion del dominio base
+@   IN  A   $IP_MASTER
 
+; Registros A
+$HOST_MASTER   IN  A   $IP_MASTER
 $HOST_SLAVE    IN  A   $IP_SLAVE_MAIN
+
+; Alias CNAME
+$ALIAS_MASTER  IN  CNAME $HOST_MASTER
 $ALIAS_SLAVE   IN  CNAME $HOST_SLAVE
 
 EOF
@@ -143,29 +151,29 @@ EOF
     #################################################
     # HOSTS DOMINIO PRINCIPAL
     #################################################
-
-    read -p "Numero de hosts del dominio principal: " NUM_HOSTS
+    echo
+    read -p "Numero de hosts extra del dominio principal (0 si no hay): " NUM_HOSTS
 
     declare -a HOSTNAMES
     declare -a HOSTIPS
 
-    for ((h=1; h<=NUM_HOSTS; h++))
-    do
+    if [[ $NUM_HOSTS -gt 0 ]]; then
+        for ((h=1; h<=NUM_HOSTS; h++))
+        do
+            echo "=========== HOST $h ==========="
+            read -p "Hostname (sin dominio): " HOST
+            read -p "Alias (opcional, enter para saltar): " ALIAS
+            read -p "IP host completa: " IPHOST
 
-        echo
-        echo "=========== HOST $h ==========="
+            HOSTNAMES[$h]=$HOST
+            HOSTIPS[$h]=$IPHOST
 
-        read -p "Hostname: " HOST
-        read -p "Alias: " ALIAS
-        read -p "IP host completa: " IPHOST
-
-        HOSTNAMES[$h]=$HOST
-        HOSTIPS[$h]=$IPHOST
-
-        echo "$HOST IN A $IPHOST" >> $DIRECTO
-        echo "$ALIAS IN CNAME $HOST" >> $DIRECTO
-
-    done
+            echo "$HOST IN A $IPHOST" >> $DIRECTO
+            if [[ -n "$ALIAS" ]]; then
+                echo "$ALIAS IN CNAME $HOST" >> $DIRECTO
+            fi
+        done
+    fi
 
     #################################################
     # FICHERO INVERSO DOMINIO PRINCIPAL
@@ -175,113 +183,102 @@ EOF
 
     cat > $INVERSO <<EOF
 \$TTL 604800
-
-@   IN  SOA $INVERSA.in-addr.arpa. $ADMIN. (
-            1
+@   IN  SOA $INVERSA.in-addr.arpa. $ADMIN (
+            2
             604800
             86400
             2419200
             604800 )
 
+@   IN  NS  $HOST_MASTER.$DOM.
 @   IN  NS  $HOST_SLAVE.$DOM.
 
 EOF
 
     #################################################
-    # PTR MASTER
+    # PTR MASTER Y SLAVE
     #################################################
 
     OCT_MASTER=$(echo $IP_MASTER | awk -F. '{print $4}')
-
     echo "$OCT_MASTER IN PTR $HOST_MASTER.$DOM." >> $INVERSO
 
-    #################################################
-    # PTR SLAVE
-    #################################################
-
     OCT_SLAVE=$(echo $IP_SLAVE_MAIN | awk -F. '{print $4}')
-
     echo "$OCT_SLAVE IN PTR $HOST_SLAVE.$DOM." >> $INVERSO
 
     #################################################
     # PTR HOSTS
     #################################################
 
-    for ((h=1; h<=NUM_HOSTS; h++))
-    do
+    if [[ $NUM_HOSTS -gt 0 ]]; then
+        for ((h=1; h<=NUM_HOSTS; h++))
+        do
+            HOST=${HOSTNAMES[$h]}
+            IP=${HOSTIPS[$h]}
+            OCTETO=$(echo $IP | awk -F. '{print $4}')
 
-        HOST=${HOSTNAMES[$h]}
-        IP=${HOSTIPS[$h]}
+            echo
+            echo "PTR detectado:"
+            echo "$OCTETO -> $HOST.$DOM."
 
-        OCTETO=$(echo $IP | awk -F. '{print $4}')
+            read -p "¿Añadir PTR? (s/n): " RESP
 
-        echo
-        echo "PTR detectado:"
-        echo "$OCTETO -> $HOST.$DOM."
-
-        read -p "¿Añadir PTR? (s/n): " RESP
-
-        if [[ $RESP == "s" || $RESP == "S" ]]; then
-            echo "$OCTETO IN PTR $HOST.$DOM." >> $INVERSO
-        fi
-
-    done
+            if [[ $RESP == "s" || $RESP == "S" ]]; then
+                echo "$OCTETO IN PTR $HOST.$DOM." >> $INVERSO
+            fi
+        done
+    fi
 
     #################################################
     # SUBDOMINIOS
     #################################################
+    echo
+    read -p "Numero de subdominios para $DOM (0 si no hay): " NUM_SUB
 
-    read -p "Numero de subdominios: " NUM_SUB
+    if [[ $NUM_SUB -gt 0 ]]; then
+        for ((s=1; s<=NUM_SUB; s++))
+        do
+            echo
+            echo "=========== SUBDOMINIO $s ==========="
+            read -p "Nombre subdominio (ej: subdominio1): " SUB
+            read -p "Red subdominio (ej: 1.3.5): " REDSUB
+            read -p "Mascara subdominio (ej: 24): " MASKSUB
+            read -p "IP del slave DNS en esta red: " IP_SLAVE_SUB
 
-    for ((s=1; s<=NUM_SUB; s++))
-    do
+            INV_SUB=$(echo $REDSUB | awk -F. '{print $3"."$2"."$1}')
 
-        echo
-        echo "=========== SUBDOMINIO $s ==========="
+            #################################################
+            # named.conf.local MASTER SUBDOMINIO
+            #################################################
 
-        read -p "Nombre subdominio: " SUB
-        read -p "Red subdominio (ej: 1.3.5): " REDSUB
-        read -p "Mascara subdominio: " MASKSUB
-
-        #################################################
-        # IP DEL SLAVE EN ESTA RED
-        #################################################
-
-        read -p "IP del slave DNS en esta red: " IP_SLAVE_SUB
-
-        INV_SUB=$(echo $REDSUB | awk -F. '{print $3"."$2"."$1}')
-
-        #################################################
-        # named.conf.local MASTER SUBDOMINIO
-        #################################################
-
-        cat >> $CONFIG <<EOF
+            cat >> $CONFIG <<EOF
 
 zone "$SUB.$DOM"
 {
     type master;
     file "/etc/bind/zonas/db.$SUB.$DOM";
-    allow-query { 127.0.0.1; $REDSUB.0/$MASKSUB; };
+    allow-query { any; };
     allow-transfer { $IP_SLAVE_MAIN; };
     also-notify { $IP_SLAVE_MAIN; };
+    notify yes;
 };
 
 zone "$INV_SUB.in-addr.arpa"
 {
     type master;
     file "/etc/bind/zonas/db.$REDSUB";
-    allow-query { 127.0.0.1; $REDSUB.0/$MASKSUB; };
+    allow-query { any; };
     allow-transfer { $IP_SLAVE_MAIN; };
     also-notify { $IP_SLAVE_MAIN; };
+    notify yes;
 };
 
 EOF
 
-        #################################################
-        # named.conf.local SLAVE SUBDOMINIO
-        #################################################
+            #################################################
+            # named.conf.local SLAVE SUBDOMINIO
+            #################################################
 
-        cat >> $CONFIG_SLAVE <<EOF
+            cat >> $CONFIG_SLAVE <<EOF
 
 //////////////////////////////////////////////////
 // SUBDOMINIO $SUB.$DOM
@@ -291,148 +288,134 @@ zone "$SUB.$DOM"
 {
     type slave;
     file "/etc/bind/zonas/db.$SUB.$DOM";
-    masters {$IP_MASTER;};
-    allow-query { 127.0.0.1; $REDSUB.0/$MASKSUB; };
+    masters { $IP_MASTER; };
+    allow-query { any; };
 };
 
 zone "$INV_SUB.in-addr.arpa"
 {
     type slave;
     file "/etc/bind/zonas/db.$REDSUB";
-    masters {$IP_MASTER;};
-    allow-query { 127.0.0.1; $REDSUB.0/$MASKSUB; };
+    masters { $IP_MASTER; };
+    allow-query { any; };
 };
 
 EOF
 
-        #################################################
-        # FICHERO DIRECTO SUBDOMINIO
-        #################################################
+            #################################################
+            # FICHERO DIRECTO SUBDOMINIO
+            #################################################
 
-        DIRECT_SUB="zonas/db.$SUB.$DOM"
+            DIRECT_SUB="zonas/db.$SUB.$DOM"
 
-        cat > $DIRECT_SUB <<EOF
+            cat > $DIRECT_SUB <<EOF
 \$TTL 604800
-
-@   IN  SOA $SUB.$DOM. $ADMIN. (
-            1
+@   IN  SOA $SUB.$DOM. $ADMIN (
+            2
             604800
             86400
             2419200
             604800 )
 
-@   IN  NS  $HOST_SLAVE.$SUB.$DOM.
+@   IN  NS  $HOST_MASTER.$DOM.
+@   IN  NS  $HOST_SLAVE.$DOM.
+
+@   IN  A   $IP_MASTER
 
 $HOST_SLAVE    IN  A   $IP_SLAVE_SUB
 $ALIAS_SLAVE   IN  CNAME $HOST_SLAVE
 
 EOF
 
-        #################################################
-        # HOSTS SUBDOMINIO
-        #################################################
-
-        read -p "Numero de hosts del subdominio: " NUM_HOST_SUB
-
-        declare -a SUBHOSTNAMES
-        declare -a SUBHOSTIPS
-
-        for ((hs=1; hs<=NUM_HOST_SUB; hs++))
-        do
+            #################################################
+            # HOSTS SUBDOMINIO
+            #################################################
 
             echo
-            echo "=========== HOST SUBDOMINIO $hs ==========="
+            read -p "Numero de hosts del subdominio $SUB (0 si no hay): " NUM_HOST_SUB
 
-            read -p "Hostname: " HOSTSUB
-            read -p "Alias: " ALIASSUB
-            read -p "IP host completa: " IPHOSTSUB
+            declare -a SUBHOSTNAMES
+            declare -a SUBHOSTIPS
 
-            SUBHOSTNAMES[$hs]=$HOSTSUB
-            SUBHOSTIPS[$hs]=$IPHOSTSUB
+            if [[ $NUM_HOST_SUB -gt 0 ]]; then
+                for ((hs=1; hs<=NUM_HOST_SUB; hs++))
+                do
+                    echo "=========== HOST SUBDOMINIO $hs ==========="
+                    read -p "Hostname (sin dominio): " HOSTSUB
+                    read -p "Alias (opcional): " ALIASSUB
+                    read -p "IP host completa: " IPHOSTSUB
 
-            echo "$HOSTSUB IN A $IPHOSTSUB" >> $DIRECT_SUB
-            echo "$ALIASSUB IN CNAME $HOSTSUB" >> $DIRECT_SUB
+                    SUBHOSTNAMES[$hs]=$HOSTSUB
+                    SUBHOSTIPS[$hs]=$IPHOSTSUB
 
-        done
+                    echo "$HOSTSUB IN A $IPHOSTSUB" >> $DIRECT_SUB
+                    if [[ -n "$ALIASSUB" ]]; then
+                        echo "$ALIASSUB IN CNAME $HOSTSUB" >> $DIRECT_SUB
+                    fi
+                done
+            fi
 
-        #################################################
-        # FICHERO INVERSO SUBDOMINIO
-        #################################################
+            #################################################
+            # FICHERO INVERSO SUBDOMINIO
+            #################################################
 
-        INV_SUB_FILE="zonas/db.$REDSUB"
+            INV_SUB_FILE="zonas/db.$REDSUB"
 
-        cat > $INV_SUB_FILE <<EOF
+            cat > $INV_SUB_FILE <<EOF
 \$TTL 604800
-
-@   IN  SOA $INV_SUB.in-addr.arpa. $ADMIN. (
-            1
+@   IN  SOA $INV_SUB.in-addr.arpa. $ADMIN (
+            2
             604800
             86400
             2419200
             604800 )
 
-@   IN  NS  $HOST_SLAVE.$SUB.$DOM.
+@   IN  NS  $HOST_MASTER.$DOM.
+@   IN  NS  $HOST_SLAVE.$DOM.
 
 EOF
 
-        #################################################
-        # PTR SLAVE SUBDOMINIO
-        #################################################
+            OCT_SLAVE_SUB=$(echo $IP_SLAVE_SUB | awk -F. '{print $4}')
+            echo "$OCT_SLAVE_SUB IN PTR $HOST_SLAVE.$DOM." >> $INV_SUB_FILE
 
-        OCT_SLAVE_SUB=$(echo $IP_SLAVE_SUB | awk -F. '{print $4}')
+            #################################################
+            # PTR HOSTS SUBDOMINIO
+            #################################################
 
-        echo "$OCT_SLAVE_SUB IN PTR $HOST_SLAVE.$SUB.$DOM." >> $INV_SUB_FILE
+            if [[ $NUM_HOST_SUB -gt 0 ]]; then
+                for ((ps=1; ps<=NUM_HOST_SUB; ps++))
+                do
+                    HOST=${SUBHOSTNAMES[$ps]}
+                    IP=${SUBHOSTIPS[$ps]}
+                    OCTETO=$(echo $IP | awk -F. '{print $4}')
 
-        #################################################
-        # PTR HOSTS SUBDOMINIO
-        #################################################
+                    echo
+                    echo "PTR detectado:"
+                    echo "$OCTETO -> $HOST.$SUB.$DOM."
 
-        for ((ps=1; ps<=NUM_HOST_SUB; ps++))
-        do
+                    read -p "¿Añadir PTR? (s/n): " RESP
 
-            HOST=${SUBHOSTNAMES[$ps]}
-            IP=${SUBHOSTIPS[$ps]}
-
-            OCTETO=$(echo $IP | awk -F. '{print $4}')
-
-            echo
-            echo "PTR detectado:"
-            echo "$OCTETO -> $HOST.$SUB.$DOM."
-
-            read -p "¿Añadir PTR? (s/n): " RESP
-
-            if [[ $RESP == "s" || $RESP == "S" ]]; then
-                echo "$OCTETO IN PTR $HOST.$SUB.$DOM." >> $INV_SUB_FILE
+                    if [[ $RESP == "s" || $RESP == "S" ]]; then
+                        echo "$OCTETO IN PTR $HOST.$SUB.$DOM." >> $INV_SUB_FILE
+                    fi
+                done
             fi
 
         done
+    fi
 
-    done
+    echo
+    echo "======================================================"
+    read -p "¿Deseas añadir otro dominio principal? (s/n): " RESP_DOM
+    if [[ "$RESP_DOM" != "s" && "$RESP_DOM" != "S" ]]; then
+        break
+    fi
 
+    ((d++))
 done
 
 echo
 echo "=========================================="
 echo " CONFIGURACION GENERADA CORRECTAMENTE"
 echo "=========================================="
-echo
-echo "Ficheros generados:"
-echo
-echo " - named.conf.local"
-echo " - named.conf.local.slave"
-echo " - carpeta zonas/"
-echo
-echo "Copiar MASTER:"
-echo
-echo "sudo mkdir -p /etc/bind/zonas"
-echo "sudo cp named.conf.local /etc/bind/"
-echo "sudo cp zonas/* /etc/bind/zonas/"
-echo
-echo "Copiar SLAVE:"
-echo
-echo "sudo cp named.conf.local.slave /etc/bind/named.conf.local"
-echo
-echo "Verificar:"
-echo
-echo "sudo named-checkconf"
-echo "sudo systemctl restart bind9"
+echo "Ficheros listos para copiar."
